eeaa0b64fb09a4198d7ec6052bd0758362508d79
Anyuki2 <108308876+Anyuki2@users.noreply.github.com>
2022-07-10 20:15:35 -0500

    Update Ozark.py

--- Ozark.py
+++ Ozark.py
@@ -2,6 +2,10 @@ from ast import FunctionDef
 import os 
 import sys 
 import time
+import queue
+import socket 
+import threading
+from queue import Queue
 
 os.system('cls' if os.name == 'nt' else 'clear')
 logo = '''
@@ -21,7 +25,7 @@ $$ |  $$ | $$  _/  $$  __$$ |$$ |      $$  _$$<
     '''
 menu ='''
     {1}--Ip lookup
-    {2}--
+    {2}--Port scan 
     {3}--
     {4}--
     {5}--
@@ -49,7 +53,7 @@ def select():
     try:
         choice = input("Ozark# ")
         if choice == "1":
-          d3 = input('Enter IP or Domain ')
+          d3 = input('Enter IP or Domain# ')
           os.system('cls' if os.name == 'nt' else 'clear')
           
           
@@ -70,6 +74,70 @@ $$ |  $$ |$$   ____| $$ |$$\       $$ |     $$ |  $$ |$$ |     $$  __$$ | $$ |$$
           os.system("curl ip-api.com/" + d3)
           quit()
         
+        elif choice == "2":
+            d4 = input('Ender Ip or Domain# ')
+            os.system('cls' if os.name == 'nt' else 'clear')
+            
+            Scan = '''
+$$\   $$\                                   
+$$$\  $$ |                                  
+$$$$\ $$ |$$$$$$\$$$$\   $$$$$$\   $$$$$$\  
+$$ $$\$$ |$$  _$$  _$$\  \____$$\ $$  __$$\ 
+$$ \$$$$ |$$ / $$ / $$ | $$$$$$$ |$$ /  $$ |
+$$ |\$$$ |$$ | $$ | $$ |$$  __$$ |$$ |  $$ |
+$$ | \$$ |$$ | $$ | $$ |\$$$$$$$ |$$$$$$$  |
+\__|  \__|\__| \__| \__| \_______|$$  ____/ 
+                                  $$ |      
+                                  $$ |      
+                                  \__|      
+            '''
+            print (Scan)
+            time.sleep(1)
+            print('Finding Open Ports.......')
+            time.sleep(3)
+            target = d4
+
+            queue = Queue()
+            open_ports = []
+
+
+            def portscan(port):
+                try:
+                  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                  sock.connect((target, port))
+                  return True 
+                except: 
+                    return False
+
+            def fill_queue(port_list): 
+               for port in port_list:
+                 queue.put(port)
+
+            def worker():
+                while not queue.empty():
+                    port = queue.get()
+                    if portscan(port):
+                        print("Port {} is open!" .format(port))
+                    open_ports.append(port)
+
+            port_list = range(1, 1024)
+            fill_queue(port_list)
+
+            thread_list = []
+
+            for t in range(100):
+             thread = threading.Thread(target=worker)
+             thread_list.append(thread)
+
+            for thread in thread_list:
+             thread.start()
+
+            for thread in thread_list:
+             thread.join() 
+
+             print("Open port are: ", open_ports)
+             
+
         elif choice == "99":
             os.system('cls' if os.name == 'nt' else 'clear')
             print("bye bye")
